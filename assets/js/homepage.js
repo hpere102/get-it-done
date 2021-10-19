f8ed498f1e30dea8ef06e1c672b76176eb6ad2c2
danny p <danny@dannys-MacBook-Pro.local>
2021-10-19 15:09:09 -0400

    cards added

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,39 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      displayRepos(data, user);
+    });
   });
-});
+};
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+
+
+  // get value from user input
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub usernme");
 }
 
-getUserRepos();
+};
+
+var displayRepos = function(repos, searchTerm) {
+  console.log(repos);
+  console.log(searchTerm);
+};
+
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
